a66871c2bd1cb6924b7f490853db4c0c367d114d
Oscar El <opelm1@gmail.com>
2023-06-23 18:30:56 +1000

    Create Todo class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import "./styles.css";
+import { v4 as uuidv4 } from 'uuid';
 
-const test = (surname: string) => {
-    const fullName = "Bob" + " " + surname;
-    return `hello ${fullName}`
+export default class Todo {
+    uuid: string;
+
+    title: string;
+
+    description: string;
+
+    priority: number;
+
+    dueDateTime: number;
+
+    shownDateTime: number;
+
+    isDone: boolean;
+
+    constructor(title: string, description = "", priority = 3, dueDateTime = 0, shownDateTime = 0, isDone = false) {
+        this.uuid = uuidv4();
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.dueDateTime = dueDateTime;
+        this.shownDateTime = shownDateTime;
+        this.isDone = isDone;
+    }
 }
-
-console.log(test("Dole"))
