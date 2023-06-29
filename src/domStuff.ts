b49d61401822a91cdc9bc23577cfae09671a0a3e
Oscar El <opelm1@gmail.com>
2023-06-29 11:23:34 +1000

    Add TODO.md

--- src/domStuff.ts
+++ src/domStuff.ts
@@ -435,35 +435,7 @@ function buildDOM() {
         });
     }
 
-    // TODO:
-    // - Make it so that any item with a parent is not in the inbox
-    // - Add checkbox to todos and projects for marking as done
-    // - Make it so that any isDone only shows in the logbook
-    // - Add display of due date if has it
-    // - Add display of priority if marked
-    // - Add event listeners to the main area for the following:
-    //   - Clicking on a todo should expand it to show its details
-    //   - Expanded todo should additionaly have:
-    //     - Description/Notes
-    //     - Start date
-    //     - Flag/priority button
-    //     - Delete button
-    //   - Clicking on an expanded todo should edit what is clicked on
-    //   - Clicking outside of an expanded todo should collapse it
-    //   - Clicking on a project should open it in the main area view
-    // - Add button for creating new todos and projects in main area
-    //   - New todos should be added to the inbox,
-    //      - or today if in today view
-    //      - or under parent project/area if in a project's/area's view (and marked as not in inbox)
-    // - Add button for creating new areas and projects in side area
-    // - Make a backend for saving data
-    // - NOW you can work on the look of the app
-    // - Look into adding animations (Theo's youtube video)
-    // - Look into adding drag and drop functionality for reordering todos and projects, possibly moving them to different areas/projects/views/etc.
-    //     - If too hard, look into adding a sort button for sorting by due date, priority, etc.
-    // - Look into adding checklists to todos
-    // - Look into adding search functionality
-    // - Look into adding dark mode toggle
+
 
     return {
         drawInbox,
